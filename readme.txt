05a02e1d169e28e447cb49a0acea01b452a0e524
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:26:12 -0400

    First commit

---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: LAMP 13 - SWITCH
+Published: 11/3/2012 1:49:36 PM
+Link: https://www.youtube.com/watch?v=WWoVdpzeIyo
+
+Description: Learn how to use the SWITCH statement to take the hard work out of writing huge IF ELSE statements.
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
---
+++ tut13.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<title>Untitled</title>
+</head>
+
+<body>
+
+<?php
+
+$name = "Othello";
+
+switch($name)
+{
+	case "Bob":
+	echo "You are not me!";
+	break;
+
+	case "Sally":
+	echo "You are not even a guy!";
+	break;
+
+	case "Bryan":
+	echo "It's me!";
+	break;
+
+	default:
+	echo "who are you";
+	
+}
+
+?>
+
+</body>
+</html>
